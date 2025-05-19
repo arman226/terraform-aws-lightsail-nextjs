083c0c5ceac42943d9eb7c0f4fb924465f74c29c
armando bibay jr <armanbibz@gmail.com>
2025-05-20 07:07:37 +0800

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 
 resource "aws_lightsail_instance" "nextjs_app" {
   name              = "nextjs-app"
-  availability_zone = "us-east-1a"
-  blueprint_id      = "ubuntu_20_04"
+  availability_zone = "ap-southeast-1a"
+  blueprint_id      = "ubuntu_22_04"
   bundle_id         = "micro_2_0"
   key_pair_name     = var.key_pair_name
-
-  user_data = file("init.sh")
+  user_data         = file("init.sh")
 }
 
 output "instance_ip" {
